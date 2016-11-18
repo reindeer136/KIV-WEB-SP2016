ba780d2803e6f6d5b6d084e227eb1cdc8c22a619
Lukas Radimersky <radimersky@live.com>
2016-11-18 22:10:00 +0100

    První verze přípravy na SP

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="cz-cs">
+    <head>
+        <meta charset="UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <title>Hello world</title>
+
+    </head>
+
+    <body>
+
+<?php
+echo "hello world\n\n";
+
+?>
+        
+        <div class="container">
+        <h1>My First Bootstrap Page</h1>
+        <p>This is some text.</p> 
+        </div>
+        
+        <div class="container-fluid">
+        <h1>My First Bootstrap Page</h1>
+        <p>This is some text.</p> 
+        </div>
+
+
+</body>
+
+</html>
